db8f980df8ab95d6be20ecb37ea6414f20830e10
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-08 00:37:25 +0900

    #2 [FEAT] 상단 네비게이션바

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,11 @@
+const Navigation = () => {
+  return (
+    <>
+      <div className="flex h-16 w-full items-center px-4 shadow-md">
+        <h1 className="text-2xl font-bold text-[#0D2D84]">Snowgent</h1>
+      </div>
+    </>
+  );
+};
+
+export default Navigation;
