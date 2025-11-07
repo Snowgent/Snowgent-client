7ddc1dccb5f0c02dc8c3831f294d81bde5addaf2
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-08 07:29:49 +0900

    #4 [CHORE] 폼타입변경

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'https://backendbase.site',
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'multipart/form-data',
   },
   withCredentials: false,
 });
