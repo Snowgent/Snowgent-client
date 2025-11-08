abb23abf8e5187b8f8375c6040720a8349d2d019
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-08 12:08:02 +0900

    #3 [CHORE] 서버 경로 변경

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://backendbase.site',
+  baseURL: 'https://backendbase.site',
   headers: {
     'Content-Type': 'multipart/form-data',
   },
